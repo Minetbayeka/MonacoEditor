d9b84a92a7567be8617fad7e15cb3d3d984fbd0d
bayeka binet <bayekabinet@gmail.com>
2025-05-16 13:12:02 +0100

    monaco code editor

---
+++ src/theme.js
@@ -0,0 +1,11 @@
+import { extendTheme } from "@chakra-ui/react";
+
+
+const theme = extendTheme({
+    config:{
+        initialColorMode :"dark",
+        useSyestemColorMode:false
+    }
+})
+
+export default theme
